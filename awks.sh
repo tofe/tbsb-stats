6e1ba52c48a63b87138e76268a0f90db7d528cc4
tofe <damien.tofan@gmail.com>
2011-12-25 00:33:17 +0000

    Revert 1fe857ab3c3129b8b6079029f2e4946979568254^..HEAD

--- awks.sh
+++
@@ -1,9 +0,0 @@
-INPUTFILE=$1
-OUTPUTFILE=$2
-awk '{ for (i = 1; i <= NF; i++) f[i] = f[i] " " $i ;
-       if (NF > n) n = NF }
- END { for (i = 1; i <= n; i++) sub(/^  */, "", f[i]) ;
-       for (i = 1; i <= n; i++) print f[i] }
-    ' $INPUTFILE >> $OUTPUTFILE
-    #' GetMemoryUsage.csv > GetMemoryUsage.csv.sorted
-
