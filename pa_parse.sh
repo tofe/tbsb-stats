e1f07cd8bbca35155d92e8771e4a530ecffe898a
tofe <damien.tofan@gmail.com>
2011-12-25 15:00:52 +0000

    Final Version of PA Script used to take output dir param too

--- pa_parse.sh
+++ pa_parse.sh
@@ -1,25 +1,35 @@
 INPUTLOG=$1
 HOSTNAME=$2
 PAPORT=$3
+OUTPUTDIR=$4
 #DATEOFTEST=15122011
 #TEST=GetSCV
 
+#Source Log File Directory
+#SOURCEDIR=`pwd $INPUTLOG`
+#echo $SOURCEDIR
 
 if [ -z "$1" ]
 then
-  echo "No input file"; echo "usage <script.sh> file_name host_name pa_port"
+  echo "No input file"; echo "usage <script.sh> file_name host_name pa_port OUTPUT_DIR"
   exit	
 fi 
 
 if [ -z "$2" ]
 then
-  echo "Hostname is null." ; echo "usage <script.sh> file_name host_name pa_port"
+  echo "No input file"; echo "usage <script.sh> file_name host_name pa_port OUTPUT_DIR"
   exit	
 fi 
 
 if [ -z "$3" ]
 then
-  echo "Policy Agent Port is null" ; echo "usage <script.sh> file_name host_name pa_port"
+  echo "No input file"; echo "usage <script.sh> file_name host_name pa_port OUTPUT_DIR"
+  exit	
+fi 
+
+if [ -z "$4" ]
+then
+  echo "No input file"; echo "usage <script.sh> file_name host_name pa_port OUTPUT_DIR"
   exit	
 fi 
 
@@ -81,23 +91,11 @@ grep minSpareThreads Connector.txt | awk '{print $3}' | sed 's/\;/\,/g' | sed -n
 ##Turn rows in to CSV columns
 echo "Time, CommittedHeap, InitialHeap, MaxHeap, UsedHeap, threadCount, peakThreadCount, DeamonThreadCount, CurrentThreadCount, CurrentThreadsBusy, MaxThreads, ConnectorAcceptCount, ConnectorMaxThreads, ConnectorMinSpareThreads" >> $HOSTNAME$PAPORT"_Totals.csv"
 ./awks.sh Get.csv.tmp $HOSTNAME$PAPORT"_Totals.csv"
- 
-#echo "Time, ThreadCount, PeakThreadCount, DaemonThreadCount" >> $HOSTNAME$PAPORT"_Threading.csv"
-#./awks.sh  GetThreading.csv.tmp $HOSTNAME$PAPORT"_Threading.csv"
- 
-#echo "Time, CurrentThreadCount, currentThreadsBusy, maxThreads" >> $HOSTNAME$PAPORT"_ThreadPool.csv"
-#./awks.sh  GetThreadPool.csv.tmp $HOSTNAME$PAPORT"_ThreadPool.csv"
- 
-#echo "Time, acceptCount, maxThreads, minSpareThreads" >> $HOSTNAME$PAPORT"_Connector.csv"
-#./awks.sh  GetThreadPool.csv.tmp $HOSTNAME$PAPORT"_Connector.csv"
- 
-#echo "Time, Total Bytes, Free Bytes, UsedBytes, PercentUsed" >>  $HOSTNAME"_MemoryUsage.csv"
-#./awks.sh GetMemoryUsage.csv.tmp $HOSTNAME"_MemoryUsage.csv"
- 
-#echo "Time,FreePhysicalMemorySize" >> $HOSTNAME"_OperatingSystem.csv"
-#./awks.sh GetOperatingSystem.csv.tmp $HOSTNAME"_OperatingSystem.csv"
-#clean up
+
+mkdir -p $OUTPUTDIR/output
  
 rm -f *.tmp
 rm -f *.txt
 rm -f TimeStamps.csv
+
+mv  $HOSTNAME$PAPORT"_Totals.csv" $OUTPUTDIR/output/
