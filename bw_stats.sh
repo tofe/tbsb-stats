080a6d5197619079368c353dafd1e3e29fbc85fc
tofe <damien.tofan@gmail.com>
2011-12-25 12:41:23 +0000

    BW Stats from ukirtbb5

---
+++ bw_stats.sh
@@ -0,0 +1,70 @@
+INPUTLOG=bwstats.log
+BWHOSTNAME=ukirsbl4
+DATEOFTEST=11122011
+TEST=GetSCV
+ 
+#Get Time Stamps
+for i in `egrep '[0-9][0-9]:[0-9][0-9]' $INPUTLOG`; do echo $i"," ; done >> timestamps.txt
+ 
+# Take relevent fields
+grep -A 4 ^HeapMemoryUsage $INPUTLOG | egrep '(committed|init|max|used)' >> HeapMemory.txt
+grep -A 6 Threading $INPUTLOG  | grep -v \# | grep -v \- | grep -v "^$"  >> Threading.txt
+grep -A 1 OperatingSystem $INPUTLOG  | grep -v \-  | grep Free >> OperatingSystem.txt
+grep Threads $INPUTLOG  | grep -v max | grep -v current | grep -v min >> threads.txt
+grep -A 7 GetMemoryUsage $INPUTLOG | egrep '(FreeBytes|PercentUsed|TotalBytes|UsedBytes)' >> GetMemoryUsage.txt
+grep -A 3 Connector $INPUTLOG  | egrep '(acceptCount|maxThreads)' >> TomCatConnector.txt
+grep -A 6 ThreadPool $INPUTLOG  | grep -v \# | grep -v \- | grep -v "^$"  >> ThreadPool.txt
+ 
+# Parse Timestamps to CSV
+cat timestamps.txt | sed -n -e ":a" -e "$ s/\n/ /gp;N;b a" >> TimeStamps.csv
+ 
+#Collect  "Time, CommittedBytes, InitialBytes, MaxBytes, Used" >> GetHeapMemoryUsage.csv
+cat TimeStamps.csv >> GetHeapMemoryUsage.csv.tmp
+grep committed HeapMemory.txt | awk '{print $3}' | sed 's/\;/\,/g' | sed -n -e ":a" -e "$ s/\n/ /gp;N;b a" >> GetHeapMemoryUsage.csv.tmp
+grep init HeapMemory.txt | awk '{print $3}' | sed 's/\;/\,/g' | sed -n -e ":a" -e "$ s/\n/ /gp;N;b a" >> GetHeapMemoryUsage.csv.tmp
+grep max HeapMemory.txt | awk '{print $3}' | sed 's/\;/\,/g' | sed -n -e ":a" -e "$ s/\n/ /gp;N;b a" >> GetHeapMemoryUsage.csv.tmp
+grep used HeapMemory.txt | awk '{print $3}' | sed 's/\;/\,/g' | sed -n -e ":a" -e "$ s/\n/ /gp;N;b a" >> GetHeapMemoryUsage.csv.tmp
+ 
+#Collect  "Time, Total Bytes, Free Bytes, UsedBytes, PercentUsed" >> GetMemoryUsage.csv
+cat TimeStamps.csv >> GetMemoryUsage.csv.tmp
+grep TotalBytes GetMemoryUsage.txt | awk '{print $3}' | sed 's/\;/\,/g' | sed -n -e ":a" -e "$ s/\n/ /gp;N;b a" >> GetMemoryUsage.csv.tmp
+grep FreeBytes GetMemoryUsage.txt | awk '{print $3}' | sed 's/\;/\,/g' | sed -n -e ":a" -e "$ s/\n/ /gp;N;b a" >> GetMemoryUsage.csv.tmp
+grep UsedBytes GetMemoryUsage.txt | awk '{print $3}' | sed 's/\;/\,/g' | sed -n -e ":a" -e "$ s/\n/ /gp;N;b a" >> GetMemoryUsage.csv.tmp
+grep PercentUsed GetMemoryUsage.txt | awk '{print $3}' | sed 's/\;/\,/g' | sed -n -e ":a" -e "$ s/\n/ /gp;N;b a" >> GetMemoryUsage.csv.tmp
+ 
+#Collect "FreePhysicalMemorySize" >> GetOperatingSystem.csv
+cat TimeStamps.csv >> GetOperatingSystem.csv.tmp
+grep FreePhysicalMemorySize OperatingSystem.txt | awk '{print $3}' | sed 's/\;/\,/g' | sed -n -e ":a" -e "$ s/\n/ /gp;N;b a" >> GetOperatingSystem.csv.tmp
+ 
+#Collect "PeakThreadCount, DaemonThreadCount" >> GetThreading.csv
+cat TimeStamps.csv >> GetThreading.csv.tmp
+grep PeakThreadCount Threading.txt | awk '{print $3}' | sed 's/\;/\,/g' | sed -n -e ":a" -e "$ s/\n/ /gp;N;b a" >> GetThreading.csv.tmp
+grep DaemonThreadCount Threading.txt | awk '{print $3}' | sed 's/\;/\,/g' | sed -n -e ":a" -e "$ s/\n/ /gp;N;b a" >> GetThreading.csv.tmp
+ 
+ 
+#Collect "maxThreads, currentThreadCount, currentThreadsBusy " >> GetThreadPool.csv.tmp
+cat TimeStamps.csv >> GetThreadPool.csv.tmp
+grep maxThreads ThreadPool.txt | awk '{print $3}' | sed 's/\;/\,/g' | sed -n -e ":a" -e "$ s/\n/ /gp;N;b a" >> GetThreadPool.csv.tmp
+grep currentThreadCount ThreadPool.txt | awk '{print $3}' | sed 's/\;/\,/g' | sed -n -e ":a" -e "$ s/\n/ /gp;N;b a" >> GetThreadPool.csv.tmp
+grep currentThreadsBusy ThreadPool.txt | awk '{print $3}' | sed 's/\;/\,/g' | sed -n -e ":a" -e "$ s/\n/ /gp;N;b a" >> GetThreadPool.csv.tmp
+ 
+ 
+#Turn rows in to CSV columns
+echo "Time, CommittedBytes, InitialBytes, MaxBytes, Used" >> $BWHOSTNAME"_HeapMemoryUsage.csv"
+./awks.sh GetHeapMemoryUsage.csv.tmp $BWHOSTNAME"_HeapMemoryUsage.csv"
+ 
+echo "Time, Total Bytes, Free Bytes, UsedBytes, PercentUsed" >>  $BWHOSTNAME"_MemoryUsage.csv"
+./awks.sh GetMemoryUsage.csv.tmp $BWHOSTNAME"_MemoryUsage.csv"
+ 
+echo "Time,FreePhysicalMemorySize" >> $BWHOSTNAME"_OperatingSystem.csv"
+./awks.sh GetOperatingSystem.csv.tmp $BWHOSTNAME"_OperatingSystem.csv"
+ 
+echo "Time, PeakThreadCount, DaemonThreadCount" >> $BWHOSTNAME"_Threading.csv"
+./awks.sh  GetThreading.csv.tmp $BWHOSTNAME"_Threading.csv"
+ 
+echo "Time, maxThreads, CurrentThreadCount, CurrentThreadsBusy" >> $BWHOSTNAME"_ThreadPool.csv"
+./awks.sh  GetThreadPool.csv.tmp $BWHOSTNAME"_ThreadPool.csv"
+ 
+#clean up
+rm -f *.tmp
+rm -f *.txt
